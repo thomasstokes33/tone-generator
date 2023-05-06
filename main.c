30a7a19f130edfc845e296bf00487cd9ce13a748
tstokes <thomasstokes@yahoo.co.uk>
2023-05-06 12:50:53 +0100

    Make buzzer work

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 #include "main.h"
-
-
+#include <stdint.h>
 
 int playNote(uint32_t gpio, float frequency) {
      gpio_set_function(buzzerPin, GPIO_FUNC_PWM);
     
     uint32_t slice = pwm_gpio_to_slice_num(buzzerPin);
     uint32_t clock_freq = clock_get_hz(clk_sys);
-    uint64_t divider = 1;
-    uint32_t wrap = clock_freq /(divider * frequency);
+    uint32_t divider = 1;
+    
+    uint32_t wrap = clock_freq /(frequency);
+    while (wrap>65535) {
+        divider *= 2;
+        wrap = clock_freq/(frequency) /divider;
 
+    }
 
     pwm_set_wrap(slice, wrap); //sets the value to wrap to 0 at
     pwm_set_clkdiv(slice, divider); //sets the divider which is when the counter increments: sysclock/divider
-    pwm_set_chan_level(slice, PWM_CHAN_A, 1); //counter compare value for a channel
+    pwm_set_chan_level(slice, PWM_CHAN_A, wrap/2); //counter compare value for a channel. the time spent at high is proportional to the this value. 
     
     
     
@@ -44,8 +48,8 @@ bool flash(struct repeating_timer *t) {
 
 int main() {
    stdio_init_all();
-   
-   playNote(buzzerPin, 100000.5f);
+   sleep_ms(3000);
+   playNote(buzzerPin, 10000.5f);
    gpio_init(16);
    gpio_set_dir(16, GPIO_OUT);
    
